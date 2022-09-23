a824ada470b7f0b4e8285180e26546cd14c44fe3
Paul2D <paul.maxineanu@gmail.com>
2022-09-23 20:28:57 +0300

    add light switcher

---
+++ src/components/DarkMode.vue
@@ -0,0 +1,15 @@
+<template>
+    <button
+        @click="toggleDark()"
+        class="switch-theme"
+    >{{ isDark ? 'light is off' : 'light is on' }}</button>
+</template>
+
+<script setup>
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark({
+    selector: 'html'
+})
+const toggleDark = useToggle(isDark)
+</script>
